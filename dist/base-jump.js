e6a703db5de3c51ba04250b50729b7319d1cdd6c
Johnny Ezzell <johnnyezzell@yahoo.com>
2014-07-17 00:38:45 -0400

    Revert "Added the insertBefore function to Array"
    This reverts commit 8632e0720217c9db9b13bbc7dfa5ce8afdb7ae23.

--- dist/base-jump.js
+++
@@ -1,8 +0,0 @@
-/*
-base-jump 0.0.0 - A leap forward for your base JavaScript objects
-Built on 2014-07-17
-*/
-Array.prototype.insertBefore = function(index, item) {
-    this.splice(index, 0, item);
-};
-;
--- src/array.js
+++
@@ -1,3 +0,0 @@
-Array.prototype.insertBefore = function(index, item) {
-    this.splice(index, 0, item);
-};
