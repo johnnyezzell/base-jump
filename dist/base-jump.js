4855dbab10299dee08ad61697232f05b3c288db1
Johnny Ezzell <johnnyezzell@yahoo.com>
2014-07-18 00:49:45 -0400

    Fixed issue with build

--- dist/base-jump.js
+++ dist/base-jump.js
@@ -2,3 +2,30 @@
 base-jump 0.0.0 - A leap forward for your base JavaScript objects
 Built on 2014-07-18
 */
+// Insert an item in an array before the index passed
+// Throws "Index out of range" error
+Array.prototype.insertBefore = function(index, item) {
+    if (index > this.length - 1 || index < 0) {
+        throw new Error("Index out of range");
+    }
+    this.splice(index, 0, item);
+};
+
+// Insert an itme into an array after the index passed
+// Throws "Index out of range" error
+Array.prototype.insertAfter = function(index, item) {
+    if (index > this.length - 1 || index < 0) {
+        throw new Error("Index out of range");
+    }
+    this.splice(index + 1, 0, item);
+};
+;
+var BJ = function(obj) {
+    if (obj instanceof BJ) return obj;
+    if (!(this instanceof BJ)) return new BJ(obj);
+    this._wrapped = obj;
+};
+
+BJ.isFunction = function(obj) {
+    return (typeof obj === 'function');
+};
--- gruntfile.js
+++ gruntfile.js
@@ -19,7 +19,7 @@ module.exports = function(grunt) {
             },
             build: {
                 files: [{
-                    src: ['src/*/**.js'],
+                    src: ['src/**/*.js'],
                     dest: 'dist/<%= pkg.name %>.js'
                 }]
             }
@@ -43,13 +43,11 @@ module.exports = function(grunt) {
             }
         }
     });
-    
-    
+        
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-jasmine');
   
-    grunt.registerTask('default',
-                       ['jshint', 'jasmine', 'concat', 'uglify']);
+    grunt.registerTask('default', ['jshint', 'jasmine', 'concat', 'uglify']);
 };
